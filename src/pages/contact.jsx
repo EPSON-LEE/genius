cf05d2288e9cba2d9814427eb660e6c69284a006
lijiahao <lijiahao@keep.com>
2019-07-01 15:14:36 +0800

    add typing

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,4 +1,15 @@
 import React from "react"
 import Layout from "../components/Layout/layout"
+import contractCss from "./style/contract.module.css"
 
-export default () => <Layout />
+export default () => {
+  return (
+    <Layout>
+      <div className="contractCss.container">
+        <span className={contractCss.profile} />
+        {/* typing animation */}
+        <p className={contractCss.typing}>男, 24岁 33232333232 3232sddsssdssdds,dsddsadadadsddsa3343242343</p>
+      </div>
+    </Layout>
+  )
+}
